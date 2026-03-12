37c8238cb9f4479b842481488497d025fc470b83
Klajdimuzhaqi <klajdimuzhaqi8@gamil.com>
2026-03-12 17:18:03 +0100

    update

---
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+import Notifications from './Notifications';
+import { getCurrentYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="root-notifications">
+        <Notifications />
+      </div>
+
+      <div className="App-header">
+        <h1>School dashboard</h1>
+      </div>
+
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
+
+        <button type="button">OK</button>
+      </div>
+
+      <div className="App-footer">
+        <p>
+          Copyright {getCurrentYear()} - {getFooterCopy(true)}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
