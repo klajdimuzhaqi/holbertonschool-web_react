bb65667870ae9956a0372af5aaf37334976e27b6
Klajdimuzhaqi <klajdimuzhaqi8@gamil.com>
2026-03-12 18:10:09 +0100

    update

--- react_intro/task_4/dashboard/vite.config.js
+++ react_intro/task_4/dashboard/vite.config.js
@@ -1,8 +1,8 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/holbertonschool-web_react/",
+  base: '/holbertonschool-web_react/react_intro/task_4/dashboard/'
 })
